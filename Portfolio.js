2c4c48d2c7cf5c6ff4b89c380ea0d34a6fd77b4b
DEEPAMJYOTI MOHANTY <likun2473@gmail.com>
2024-08-09 21:31:10 +0530

    Add files via upload
    This project was developed as part of my internship at CodSoft. It is a [briefly describe the type of project, e.g., "full-stack job board application," PORTFOLIO,LANDING,PAGE,CALCULATOR" etc.] built using HTML,CSS, and JAVA SCRIPT. 
    Technologies Used:
    Frontend: React, HTML, CSS, JavaScript
    Version Control: Git & GitHub,Visual Studio

---
+++ Portfolio.js
@@ -0,0 +1,17 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    let modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
